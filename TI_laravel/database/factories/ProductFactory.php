d0cb2e3b73f1acb96755a725dab97752c7c06c31
ju1618 <juliaefernandez@gmail.com>
2019-07-21 17:52:00 -0300

    nada nuevo

--- TI_laravel/database/factories/ProductFactory.php
+++ TI_laravel/database/factories/ProductFactory.php
@@ -1,5 +1,5 @@
 <?php
-/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
 use App\Product;
 use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
@@ -8,7 +8,6 @@ $factory->define(Product::class, function (Faker $faker) {
 	return [
     'name' => $faker->sentence(5, true),
     'price' => $faker->randomFloat(2, 100, 999999),
-    // 'image' => $faker->imageUrl(300, 200, 'cats'),
 		'image' => $faker->image($filePath, 400, 300, null, false)
   ];
 });
