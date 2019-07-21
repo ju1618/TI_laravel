4e982bee4a3744360f73f66f90c99a69e479f609
ju1618 <juliaefernandez@gmail.com>
2019-07-21 19:15:45 -0300

    tocando los fillable de user y product

--- TI_laravel/database/factories/ProductFactory.php
+++ TI_laravel/database/factories/ProductFactory.php
@@ -10,6 +10,8 @@ $factory->define(Product::class, function (Faker $faker) {
 	return [
     'name' => $faker->sentence(5, true),
     'price' => $faker->randomFloat(2, 100, 999999),
-		'image' => $faker->image($filePath, 400, 300, null, false)
+		'image' => $faker->image($filePath, 400, 300, null, false),
+    'user_id'=> $faker->randomDigitNotNull ,
+    'category' => $faker->randomElement($array = array ('Cocina','Oficina','Hogar'))
   ];
 });
