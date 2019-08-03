ebd308d8db8d1988ea06593be7a221614af72d2a
Romi07 <rgramirezuba@gmail.com>
2019-08-03 09:54:36 -0300

    subo perfil y parte de carrito de compras

--- TI_laravel/routes/web.php
+++ TI_laravel/routes/web.php
@@ -10,10 +10,39 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Para buscar los productos por el slug
+Route::bind('product', function($slug){
+   return App\Product::where('slug', $slug)->first();
+});
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+//Ruta de perfil de usuario
+Route::get('/perfil', 'PerfilController@index')->middleware('auth');
+
+//Modificaciones del perfil de usuario
+Route::resource('perfil', 'PerfilController');
+Route::resource('perfil/create', 'PerfilController@create');
+Route::post('altaperfil', ['as' => 'perfil.store', 'uses' => 'PerfilController@store']);
+
+//Metodo de pago: Paypal
+
+//Enviamos el pedido a Paypal
+Route::get('payment', array('as' => 'payment', 'uses' => 'PaypalController@postPayment',));
+
+//Después de realizar el pago Paypal redirecciona a esta Ruta
+Route::get('payment/status', array('as' => 'payment.status', 'uses' => 'PaypalController@getPaymentStatus',));
+
+//---Carrito---
+Route::get('cart/show', ['as'=>'cart-show', 'uses' => 'CartController@show']);
+Route::get('cart/add/{product}', ['as'=> 'cart-add', 'uses' => 'CartController@add']);
+Route::get('cart/delete/{product}', ['as'=> 'cart-delete', 'uses' => 'CartController@delete']);
+Route::get('cart/trash', ['as'=> 'cart-trash', 'uses' => 'CartController@trash']);
+Route::get('cart/update/{product}', ['as'=> 'cart-update', 'uses' => 'CartController@update']);
+Route::get('order-detail', ['middleware'=>'auth:user', 'as'=>'order-detail', 'uses'=>'CartController@orderDetail']);
+
 
 // Rutas de Products
 Route::get('/',  'ProductsController@index');
@@ -25,6 +54,10 @@ Route::post('/products/addProduct', 'ProductsController@store');
 Route::get('/listado',  'ProductsController@listar');
 
 Route::get('/faq', 'FaqController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
